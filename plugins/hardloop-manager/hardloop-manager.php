332a58f862ae83ba662d57179c580a61b5181f78
Virgil Smit <Virgilsmit@gmail.com>
2025-04-30 18:20:25 +0200

    Register ACF field group Loper-gegevens

--- plugins/hardloop-manager/hardloop-manager.php
+++ plugins/hardloop-manager/hardloop-manager.php
@@ -13,3 +13,65 @@
  */
 
 // Your code starts here.
+require_once __DIR__ . '/src/acf.php';
+
+add_action('init', function() {
+    // Register Custom Post Type: loper
+    $labels = [
+        'name'                  => _x('Lopers', 'Post Type General Name', 'hardloop-manager'),
+        'singular_name'         => _x('Loper', 'Post Type Singular Name', 'hardloop-manager'),
+        'menu_name'             => __('Lopers', 'hardloop-manager'),
+        'name_admin_bar'        => __('Loper', 'hardloop-manager'),
+        'add_new'               => __('Add New', 'hardloop-manager'),
+        'add_new_item'          => __('Add New Loper', 'hardloop-manager'),
+        'edit_item'             => __('Edit Loper', 'hardloop-manager'),
+        'new_item'              => __('New Loper', 'hardloop-manager'),
+        'view_item'             => __('View Loper', 'hardloop-manager'),
+        'search_items'          => __('Search Lopers', 'hardloop-manager'),
+        'not_found'             => __('No lopers found', 'hardloop-manager'),
+        'not_found_in_trash'    => __('No lopers found in Trash', 'hardloop-manager'),
+        'all_items'             => __('All Lopers', 'hardloop-manager'),
+        'archives'              => __('Loper Archives', 'hardloop-manager'),
+        'insert_into_item'      => __('Insert into loper', 'hardloop-manager'),
+        'uploaded_to_this_item' => __('Uploaded to this loper', 'hardloop-manager'),
+    ];
+
+    $args = [
+        'label'               => __('Loper', 'hardloop-manager'),
+        'labels'              => $labels,
+        'public'              => true,
+        'has_archive'         => true,
+        'show_in_rest'        => true,
+        'menu_icon'           => 'dashicons-running',
+        'supports'            => ['title', 'editor', 'thumbnail'],
+        'capability_type'     => ['loper', 'lopers'],
+        'map_meta_cap'        => true,
+    ];
+    register_post_type('loper', $args);
+
+    // Register Taxonomy: groep
+    $tax_labels = [
+        'name'              => _x('Groepen', 'taxonomy general name', 'hardloop-manager'),
+        'singular_name'     => _x('Groep', 'taxonomy singular name', 'hardloop-manager'),
+        'search_items'      => __('Search Groepen', 'hardloop-manager'),
+        'all_items'         => __('All Groepen', 'hardloop-manager'),
+        'parent_item'       => __('Parent Groep', 'hardloop-manager'),
+        'parent_item_colon' => __('Parent Groep:', 'hardloop-manager'),
+        'edit_item'         => __('Edit Groep', 'hardloop-manager'),
+        'update_item'       => __('Update Groep', 'hardloop-manager'),
+        'add_new_item'      => __('Add New Groep', 'hardloop-manager'),
+        'new_item_name'     => __('New Groep Name', 'hardloop-manager'),
+        'menu_name'         => __('Groepen', 'hardloop-manager'),
+    ];
+
+    $tax_args = [
+        'hierarchical'      => true,
+        'labels'            => $tax_labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'show_in_rest'      => true,
+        'rewrite'           => ['slug' => 'groep'],
+    ];
+    register_taxonomy('groep', ['loper'], $tax_args);
+});
---
+++ plugins/hardloop-manager/src/acf.php
@@ -0,0 +1,30 @@
+<?php
+// Load this file from hardloop-manager.php via require_once
+
+if( function_exists('acf_add_local_field_group') ):
+
+acf_add_local_field_group([
+  'key' => 'group_loper_geg',
+  'title' => 'Loper-gegevens',
+  'fields' => [
+    [ 'key'=>'field_voornaam','label'=>'Voornaam','name'=>'voornaam','type'=>'text' ],
+    [ 'key'=>'field_achternaam','label'=>'Achternaam','name'=>'achternaam','type'=>'text' ],
+    [ 'key'=>'field_email','label'=>'E-mail','name'=>'email','type'=>'email' ],
+    [ 'key'=>'field_geboortedatum','label'=>'Geboortedatum','name'=>'geboortedatum','type'=>'date_picker' ],
+    [ 'key'=>'field_noodnummer','label'=>'Noodnummer','name'=>'noodnummer','type'=>'text' ],
+    [ 'key'=>'field_loopervaring','label'=>'Loopervaring','name'=>'loopervaring','type'=>'textarea' ],
+    [ 'key'=>'field_gewenst_pace','label'=>'Gewenst pace','name'=>'gewenst_pace','type'=>'text' ],
+    [ 'key'=>'field_doel_2025','label'=>'Doel 2025','name'=>'doel_2025','type'=>'textarea' ],
+    [ 'key'=>'field_shirt','label'=>'Shirtmaat','name'=>'shirt','type'=>'text' ],
+    [ 'key'=>'field_blessures','label'=>'Blessures','name'=>'blessures','type'=>'textarea' ],
+    [ 'key'=>'field_opmerkingen','label'=>'Opmerkingen','name'=>'opmerkingen','type'=>'textarea' ],
+    [ 'key'=>'field_trainer_opmerkingen','label'=>'Trainer opmerkingen','name'=>'trainer_opmerkingen','type'=>'textarea' ],
+    [ 'key'=>'field_niveau','label'=>'Niveau','name'=>'niveau','type'=>'select','choices'=>['beginner'=>'Beginner','gevorderd'=>'Gevorderd','expert'=>'Expert'] ],
+    [ 'key'=>'field_actief','label'=>'Actief','name'=>'actief','type'=>'true_false','ui'=>1 ],
+    [ 'key'=>'field_gestart','label'=>'Gestart','name'=>'gestart','type'=>'true_false','ui'=>1 ],
+  ],
+  'location' => [[ [ 'param'=>'post_type','operator'=>'==','value'=>'loper' ] ]],
+  'menu_order'=>0,'position'=>'normal','style'=>'default','label_placement'=>'top','instruction_placement'=>'label',
+]);
+
+endif;
