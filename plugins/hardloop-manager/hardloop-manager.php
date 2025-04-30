0264692166cb6d61e9b21e13c05a394b72a6531c
Virgil Smit <Virgilsmit@gmail.com>
2025-04-30 15:32:54 +0200

    Add plugins folder & scaffold hardloop-manager

---
+++ plugins/hardloop-manager/hardloop-manager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Hardloop Manager
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     hardloop-manager
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Hardloop_Manager
+ */
+
+// Your code starts here.
